e4e8d5ea02c4ee974c48ea9cfd02ff6bbcca3094
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-06-27 10:52:21 -0300

    Refactor arithmetic operations in Form1.cs with a single method.

--- OperadoresAritmeticos/Form1.cs
+++ OperadoresAritmeticos/Form1.cs
@@ -9,42 +9,51 @@ namespace OperadoresAritmeticos
 
         private void btnSomar_Click(object sender, EventArgs e)
         {
-            int numero1 = txbn1.Text == "" ? 0 : Convert.ToInt32(txbn1.Text);
-            int numero2 = txbn2.Text == "" ? 0 : Convert.ToInt32(txbn2.Text);
-            int resultado = numero1 + numero2;
-            txbresult.Text = resultado.ToString();
+            txbresult.Text = operadoresAritmeticos(Convert.ToInt32(txbn1.Text), Convert.ToInt32(txbn2.Text), "+").ToString();
         }
 
         private void btnDividir_Click(object sender, EventArgs e)
         {
-            int numero1 = txbn1.Text == "" ? 0 : Convert.ToInt32(txbn1.Text);
-            int numero2 = txbn2.Text == "" ? 0 : Convert.ToInt32(txbn2.Text);
-            int resultado = numero1 / numero2;
-            txbresult.Text = resultado.ToString();
+            txbresult.Text = operadoresAritmeticos(Convert.ToInt32(txbn1.Text), Convert.ToInt32(txbn2.Text), "/").ToString();
         }
 
         private void btnRestoDivisao_Click(object sender, EventArgs e)
         {
-            int numero1 = txbn1.Text == "" ? 0 : Convert.ToInt32(txbn1.Text);
-            int numero2 = txbn2.Text == "" ? 0 : Convert.ToInt32(txbn2.Text);
-            int resultado = numero1 % numero2;
-            txbresult.Text = resultado.ToString();
+            txbresult.Text = operadoresAritmeticos(Convert.ToInt32(txbn1.Text), Convert.ToInt32(txbn2.Text), "%").ToString();
         }
 
         private void btnMultiplicar_Click(object sender, EventArgs e)
         {
-            int numero1 = txbn1.Text == "" ? 0 : Convert.ToInt32(txbn1.Text);
-            int numero2 = txbn2.Text == "" ? 0 : Convert.ToInt32(txbn2.Text);
-            int resultado = numero1 * numero2;
-            txbresult.Text = resultado.ToString();
+            txbresult.Text = operadoresAritmeticos(Convert.ToInt32(txbn1.Text), Convert.ToInt32(txbn2.Text), "*").ToString();
         }
 
         private void btnSubtrair_Click(object sender, EventArgs e)
         {
-            int numero1 = txbn1.Text == "" ? 0 : Convert.ToInt32(txbn1.Text);
-            int numero2 = txbn2.Text == "" ? 0 : Convert.ToInt32(txbn2.Text);
-            int resultado = numero1 - numero2;
-            txbresult.Text = resultado.ToString();
+            txbresult.Text = operadoresAritmeticos(Convert.ToInt32(txbn1.Text), Convert.ToInt32(txbn2.Text), "-").ToString();
+        }
+
+        int operadoresAritmeticos(int numero1, int numero2, string operador)
+        {
+            int resultado = 0;
+            switch (operador)
+            {
+                case "+":
+                    resultado = numero1 + numero2;
+                    break;
+                case "-":
+                    resultado = numero1 - numero2;
+                    break;
+                case "*":
+                    resultado = numero1 * numero2;
+                    break;
+                case "/":
+                    resultado = numero1 / numero2;
+                    break;
+                case "%":
+                    resultado = numero1 % numero2;
+                    break;
+            }
+            return resultado;
         }
     }
 }
