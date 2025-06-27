e9d995e490c4539e7f3a3e4de6470ada9e55ef2a
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-06-27 09:13:22 -0300

    Add BoxingVsUnboxing project and examples

---
+++ ConversaoDeDados/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConversaoDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Conversão explicita
+            int i;
+            double y = 10.5;
+            i = (int)y;
+            // No exemplo abaixo, a conversão pode ser feita de forma implícita, pois o tipo 'int' é menor que o tipo 'long'.    
+            long x;
+            int a = 5;
+            x = a;
+        }
+    }
+    class boxing
+    {
+        // Boxing é o processo de converter um tipo de valor em um tipo de referência.
+        // Unboxing é o processo inverso, convertendo um tipo de referência de volta para um tipo de valor.
+        public void ExemploBoxingUnboxing()
+        {
+            int valor = 10;
+            object obj = valor; // Boxing
+            int valorDesconstruido = (int)obj; // Unboxing
+        }
+    }
+}
