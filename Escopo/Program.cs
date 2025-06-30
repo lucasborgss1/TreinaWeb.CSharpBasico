8e68171ba777e0a13d0bdd9e1e2aebec41dd66ae
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-06-30 11:09:51 -0300

    Add new "Escopo" project with variable scope examples

---
+++ Escopo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Escopo
+{
+    class Exemplo
+    {
+        //    void Metodo()
+        //    {
+        //        int variavel;
+        //        //...
+        //    }
+
+        //    void OutroMetodo()
+        //    {
+        //        variavel = 42; // Isso gera um erro de compilação
+        //    } 
+        // O erro mostrado acima ocorreu porque a variável foi criada dentro de um método diferente da qual está sendo usada.
+
+        int variavel;
+        void Metodo()
+
+        {
+            variavel = 10; // Isso é permitido, pois a variável está no escopo da classe
+        }
+        void OutroMetodo()
+        {
+            variavel = 42; 
+        }
+    }
+
+}
+
+
+}
