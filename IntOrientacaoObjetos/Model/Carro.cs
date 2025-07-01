82cda54f30194138efe836b3802231b72acb4e67
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-01 11:46:43 -0300

    Add IntOrientacaoObjetos project and Carro class

---
+++ IntOrientacaoObjetos/Model/Carro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntOrientacaoObjetos.Model
+{
+    class Carro
+    {
+        public string Cor = "";
+        public string Modelo = "";
+        public string Motor = "";
+        public string Status = "";
+
+        public void Ligar()
+        {
+            Status = "Ligando o carro.";
+        }
+
+        public void Desligar()
+        {
+            Status = "Desligando o carro.";
+        }
+
+        public void Acelerar()
+        {
+            Status = "Acelerando o carro.";
+        }
+
+        public void Brecar()
+        {
+            Status = "Brecando o carro.";
+        }
+
+        public void MudarMarcha()
+        {
+            Status = "Mudando de Marcha.";
+        }
+    }
+}
---
+++ IntOrientacaoObjetos/Program.cs
@@ -0,0 +1,22 @@
+﻿using IntOrientacaoObjetos.Model;
+
+namespace IntOrientacaoObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c1 = new Carro();
+            c1.Cor = "Vermelho";
+            c1.Modelo = "Vectra";
+            c1.Motor = "1.8";
+            c1.Ligar();
+            c1.Acelerar();
+            c1.MudarMarcha();
+            c1.Brecar();
+            c1.Desligar();
+
+            Carro c2 = new Carro();
+        }
+    }
+}
