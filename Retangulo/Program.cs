aa96b1efb30155216d07b33845d86bfff5cc95bd
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-01 08:02:34 -0300

    Add Retangulo project with area and perimeter calculations

---
+++ Retangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Retangulo.Utils;
+
+namespace Retangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a altura do retângulo: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Informe a largura do retângulo: ");
+            double largura = Convert.ToDouble(Console.ReadLine());
+
+            RetanguloModel retangulo = new RetanguloModel(largura, altura);
+            double area = retangulo.CalcularArea();
+            double perimetro = retangulo.CalcularPerimetro();
+            Console.WriteLine($"A área do retângulo é: {area}");
+            Console.WriteLine($"O perímetro do retângulo é: {perimetro}");
+        }
+    }
+}
---
+++ Retangulo/Utils/RetanguloModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retangulo.Utils
+{
+    internal class RetanguloModel
+    {
+        public double largura { get; set; }
+        public double altura { get; set; }
+        public RetanguloModel(double largura, double altura)
+        {
+            this.largura = largura;
+            this.altura = altura;
+        }
+        public double CalcularArea()
+        {
+            return largura * altura;
+        }
+        public double CalcularPerimetro()
+        {
+            return 2 * (largura + altura);
+        }
+    }
+}
