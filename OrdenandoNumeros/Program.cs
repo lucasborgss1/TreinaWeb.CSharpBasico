7d827c059ff208f33c0e3f897c4beefc728aa5d2
lucasborgss1 <lucasaborgesdev1@gmail.com>
2025-07-01 08:38:18 -0300

    Add OrdenandoNumeros project with sorting functionality

---
+++ OrdenandoNumeros/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace OrdenandoNumeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o tamanho da lista:");
+            int tamanho = int.Parse(Console.ReadLine());
+            Service.ListaOrdenada lista = new Service.ListaOrdenada(tamanho);
+            double numero;
+            for (int i = 0; i < tamanho; i++)
+            {
+                Console.WriteLine($"Digite o número {i + 1}:");
+                try
+                {
+                    numero = Convert.ToDouble(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Entrada inválida. Por favor, digite um número válido.");
+                    i--;
+                    throw;
+                }
+                lista.AdicionarNumero(numero , i);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Lista antes da ordenação:");
+            lista.ExibirNumeros();
+            Console.WriteLine();
+            Console.WriteLine("Ordenando a lista...");
+            lista.OrdenarLista();
+            Console.WriteLine("Lista ordenada:");
+            lista.ExibirNumeros();
+        }
+    }
+}
---
+++ OrdenandoNumeros/Service/ListaOrdenada.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenandoNumeros.Service
+{
+    internal class ListaOrdenada
+    {
+        public int tamanho { get; set; }
+        public double[] numeros { get; set; }
+        public ListaOrdenada(int tamanho)
+        {
+            this.tamanho = tamanho;
+            numeros = new double[tamanho];
+        }
+
+        public void AdicionarNumero(double numero, int index)
+        {
+            if (index <= tamanho)
+            {
+                numeros[index] = numero;
+            }
+        }
+
+        public void ExibirNumeros()
+        {
+            foreach (var numero in numeros)
+            {
+                Console.WriteLine(numero);
+            }
+        }
+
+        public void OrdenarLista()
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                int menorIndice = i;
+                for (int j = i + 1; j < numeros.Length; j++)
+                {
+                    if (numeros[j] < numeros[menorIndice])
+                    {
+                        menorIndice = j;
+                    }
+                }
+                if (menorIndice != i)
+                {
+                    double temp = numeros[i];
+                    numeros[i] = numeros[menorIndice];
+                    numeros[menorIndice] = temp;
+                }
+            }
+        }
+    }
+}
